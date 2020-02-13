1deeb3a484ea0f10f45d8cb1e27f2a3492479d02
Nate Falconer <falconerresources@gmail.com>
2020-02-13 09:49:40 -0500

    iterations 1-3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,94 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './FoodBox'
 
 class App extends Component {
+
+
+  state = {
+    foods,
+    foodForm:false
+  }
+//  old code, redone for iteration 3
+// state = {
+//   AllFood: foods,
+//   foodForm:false
+// }
+
+// showFoods = () => {
+//   let AllFood = [...foods]
+//   return this.state.AllFood.map(eachFood => {
+//     return (
+//       <FoodBox name={eachFood.name} calories={eachFood.calories} image={eachFood.image} />
+//     )
+//   })
+// }
+
+showFoods = () => {
+  let foodList = this.state.foods.map((eachFood,i)=>{
+    return <FoodBox key = {i} {...eachFood} />
+  })
+  return foodList;
+}
+
+
+handleFormSubmit = (e) => {
+  e.preventDefault()
+  this.toggleFoodForm()
+  let newFoods = [...this.state.foods]
+  newFoods.unshift({
+    name:this.state.food,
+    calories:this.state.calories,
+    quantity:1,
+    image:this.state.image,
+  })
+
+
+  this.setState({
+    foods:newFoods
+  })
+}
+
+
+toggleFoodForm = () => {
+  this.setState({
+    foodForm: !this.state.foodForm
+  })
+}
+
+handleInputChange = (e) => {
+  console.log('change', e.target.name, e.target.value)
+  this.setState( { [e.target.name] : e.target.value } ) 
+}
+
+showFoodForm = () => {
+  if(this.state.foodForm){
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+          <input name="food" onChange={this.handleInputChange} type="text" placeholder="food name"/>
+          <input name="calories" onChange={this.handleInputChange} type="text" placeholder="calories"/>
+          <input name="image" onChange={this.handleInputChange} type="text" placeholder="image"/>
+        <input  type="submit" />
+      </form>
+      )
+    } else {
+      return <button onClick={this.toggleFoodForm}>Add New Food</button>
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="App">
+        <h1>IronNutrition</h1>
+        <br></br>
+        {this.showFoodForm()}
+        <br></br>
+
+
+        {this.showFoods()}
       </div>
     );
   }
